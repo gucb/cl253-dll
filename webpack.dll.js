36fd0b001731c9a152b5037c81a89be38793837f
bobo <bobo770880@163.com>
2020-07-31 10:18:26 +0800

    升级依赖

--- webpack.dll.js
+++ webpack.dll.js
@@ -7,25 +7,25 @@ module.exports = {
     common: ["axios", "moment"],
     react: ["react", "react-dom", "react-router-dom"],
     react_redux: ["redux", "react-redux"],
-    vue: ["vue", "vuex", "vue-router"]
+    vue: ["vue", "vuex", "vue-router"],
   },
   output: {
     path: path.resolve(__dirname, "./dll"),
     filename: "[name].dll.js",
-    library: ["[name]Dll"]
+    library: ["[name]Dll"],
   },
   plugins: [
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [path.join(__dirname, "./dll")],
-      verbose: true
+      verbose: true,
     }),
     new webpack.DllPlugin({
       context: "www.253.com",
       name: "[name]Dll",
-      path: path.join(__dirname, "./dll/[name]_manifest.json")
-    })
+      path: path.join(__dirname, "./dll/[name]_manifest.json"),
+    }),
   ],
   performance: {
-    hints: false
-  }
+    hints: false,
+  },
 };
