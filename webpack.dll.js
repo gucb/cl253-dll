9cdd4413282f7d52845f79b140756f79b45a9843
bobo <bobo770880@163.com>
2020-08-11 09:21:36 +0800

    模块改为umd

--- webpack.dll.js
+++ webpack.dll.js
@@ -13,6 +13,7 @@ module.exports = {
     path: path.resolve(__dirname, "./dll"),
     filename: "[name].dll.js",
     library: ["[name]Dll"],
+    libraryTarget: "umd",
   },
   plugins: [
     new CleanWebpackPlugin({
